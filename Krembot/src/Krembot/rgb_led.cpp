0620ca0dd7d2278d4600937b2e6b160008c1355b
Yair <yair@robotican.co>
2018-09-25 16:52:24 +0300

    re added krembot

---
+++ Krembot/src/Krembot/rgb_led.cpp
@@ -0,0 +1,50 @@
+/*******************************************************************************
+* Copyright (c) 2018, RoboTICan, LTD.
+* All rights reserved.
+*
+* Redistribution and use in source and binary forms, with or without
+* modification, are permitted provided that the following conditions are met:
+*
+* * Redistributions of source code must retain the above copyright notice, this
+*   list of conditions and the following disclaimer.
+*
+* * Redistributions in binary form must reproduce the above copyright notice,
+*   this list of conditions and the following disclaimer in the documentation
+*   and/or other materials provided with the distribution.
+*
+* * Neither the name of RoboTICan nor the names of its
+*   contributors may be used to endorse or promote products derived from
+*   this software without specific prior written permission.
+*
+* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+* AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+* IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+* DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+* FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+* DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+* SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+* CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+* OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+* OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*******************************************************************************/
+
+/* Author: Elhay Rauper */
+/* Maintainer: Yair Shlomi */
+
+#include "rgb_led.h"
+
+RGBLed::RGBLed()
+{
+  pinMode(RED_LEG ,OUTPUT);
+  pinMode(GREEN_LEG ,OUTPUT);
+  pinMode(BLUE_LEG ,OUTPUT);
+}
+
+void RGBLed::write(uint8_t red_val,
+                   uint8_t green_val,
+                   uint8_t blue_val)
+{
+  analogWrite(RED_LEG, red_val);
+  analogWrite(GREEN_LEG, green_val);
+  analogWrite(BLUE_LEG, blue_val);
+}
